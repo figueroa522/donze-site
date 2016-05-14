f5367fca2cbec0b4bac995f6c541457453de4f58
Izzy <isidrofigueroa33@gmail.com>
2016-05-14 10:04:25 -0700

    update

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ $('.stereo-tab').on('click', function(e){
 	$('.stereo-list-menu').removeClass('none');
 	doSearch('car stereo', 'stereoApiCallback', 15);
 	
-	$('.itemDisplay img').attr('src', '#');
 })
 $('.stereo-list-menu').on('change', function(e){
 	var url = $(this).find(':selected').attr('url');
@@ -63,7 +62,6 @@ $('.stereo-list-menu').on('change', function(e){
 $('.speakers-tab').on('click', function(e){
 	$('.speakers-list-menu').removeClass('none');
 	doSearch('car speakers', 'speakersApiCallback', 15);
-	$('.itemDisplay img').attr('src', '#');
 })
 $('.speakers-list-menu').on('change', function(e){
 	var url = $(this).find(':selected').attr('url');
@@ -75,7 +73,6 @@ $('.speakers-list-menu').on('change', function(e){
 $('.subwoofers-tab').on('click', function(e){
 	$('.subwoofers-list-menu').removeClass('none');
 	doSearch('car subwoofers', 'subwoofersApiCallback', 15);
-	$('.itemDisplay img').attr('src', '#');
 })
 $('.subwoofers-list-menu').on('change', function(e){
 	var url = $(this).find(':selected').attr('url');
@@ -88,7 +85,6 @@ $('.subwoofers-list-menu').on('change', function(e){
 $('.amps-tab').on('click', function(e){
 	$('.amps-list-menu').removeClass('none');
 	doSearch('car amps', 'ampApiCallback', 15);
-	$('.itemDisplay img').attr('src', '#');
 })
 $('.amps-list-menu').on('change', function(e){
 	var url = $(this).find(':selected').attr('url');
